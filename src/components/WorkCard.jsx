d37b28ee16253f8f4d068af5343c15c5d8fb62c2
Aoki Kou <aoki@gJ.localdomain>
2026-09-19 05:12:23 +0900

    Worksセクションに作品カードと技術タグを実装

--- src/components/WorkCard.jsx
+++ src/components/WorkCard.jsx
@@ -1,9 +1,26 @@
-function WorkCard({ title, description, url }) {
+function WorkCard({ title, image, description, serviceUrl, githubUrl, technologies }) {
     return (
-        <article>
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <a href={url}>詳しく見る</a>
+        <article className="work-card">
+            <img className="work-image" src={image} alt={`${title}の画面`} />
+            <div className="work-content">
+                <h3>{title}</h3>
+                <p className="work-description">
+                    {description}
+                </p>
+                <div className="technologies">
+                {technologies.map((technology) => (
+                    <span key={technology}>{technology}</span>
+                ))}
+                </div>
+                <div className="work-links">
+                    <a href={serviceUrl} className="service-link">アプリを見る</a>
+                    <a href={githubUrl} className="github-link">
+                        <img src="/images/skills/github.svg" alt="" />
+                        GitHub
+                    </a>
+                </div>
+
+            </div>
         </article>
     )
 }
